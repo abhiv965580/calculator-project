a2a3e78384d12aafdc5702ecc904c658328c53a6
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-04-04 17:19:15 +0530

    code readability improved

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let firstNumber;
+let operator;
+let secondNumber;
+let prevValue;
+let currValue = '';
+let result = 0;
+const display = document.querySelector('.display');
+const numberBtns = document.querySelectorAll('.btn.numbers');
+const operatorBtns = document.querySelectorAll('.btn.operators');
+const equalBtn = document.querySelector('.btn.equal');
+const clearBtn = document.querySelector('.btn.clear');
+const decimalBtn = document.querySelector('.btn.decimal');
+
 const add = (number1,number2) => {
     return number1 + number2;
 }
@@ -13,19 +26,6 @@ const divide = (number1,number2) => {
 const multiply = (number1,number2) => {
     return number1 * number2;
 }
-
-let firstNumber;
-let operator;
-let secondNumber;
-let prevValue;
-let currValue = '';
-let result = 0;
-const display = document.querySelector('.display');
-const numberBtns = document.querySelectorAll('.btn.numbers');
-const operatorBtns = document.querySelectorAll('.btn.operators');
-const equalBtn = document.querySelector('.btn.equal');
-const clearBtn = document.querySelector('.btn.clear');
-const decimalBtn = document.querySelector('.btn.decimal');
 
 const operate = (firstNumber,secondNumber,operator) => {
     switch (operator) {
